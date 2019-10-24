38cd14f6307dc285f1170eb0b31e01f8b725ca1d
Armanelgtron <armanelgtron@gmail.com>
2019-10-24 00:10:34 -0400

    Add Electron support

---
+++ electron.js
@@ -0,0 +1,17 @@
+const {app,BrowserWindow} = require("electron");
+
+let window; //global reference is needed for garbage collection
+
+function createWindow()
+{
+	window = new BrowserWindow({icon:"favicon.ico",backgroundColor:"#000",show:false});
+	
+	window.loadFile("index.html");
+	window.once("ready-to-show",window.show);
+	window.on("closed",function(){window=null});
+	window.autoHideMenuBar = true;
+}
+
+app.on("ready",createWindow);
+app.on("window-all-closed",app.quit);
+app.on('activate',function(){if(!window) createWindow()});
