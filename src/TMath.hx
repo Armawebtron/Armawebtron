7a854ff1badf8ad2680f1276b9c49ce6dcbf00b3
Armanelgtron <armanelgtron@gmail.com>
2026-07-14 13:01:22 -0400

    minor math optimization

--- src/TMath.hx
+++ src/TMath.hx
@@ -84,10 +84,11 @@ class TMath
 	):Bool
 	{
 		var s1_x = p1_x - p0_x, s1_y = p1_y - p0_y, s2_x = p3_x - p2_x, s2_y = p3_y - p2_y;
-		var s = (-s1_y * (p0_x - p2_x) + s1_x * (p0_y - p2_y)) / (-s2_x * s1_y + s1_x * s2_y);
-		var t = (s2_x * (p0_y - p2_y) - s2_y * (p0_x - p2_x)) / (-s2_x * s1_y + s1_x * s2_y);
-		
-		return (s >= 0 && s <= 1 && t >= 0 && t <= 1);
+		var d = ( s1_x * s2_y - s2_x * s1_y );
+		var s = (-s1_y * (p0_x - p2_x) + s1_x * (p0_y - p2_y)) / d;
+		if( s < 0 || s > 1 ) return false;
+		var t = ( s2_x * (p0_y - p2_y) - s2_y * (p0_x - p2_x)) / d;
+		return ( t >= 0 && t <= 1 );
 	}
 
 	public static function pointDistance(
